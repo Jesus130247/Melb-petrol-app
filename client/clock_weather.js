c0dca3df6b65d964e93f27408e57e6e3c816e16e
Alexander <alexanderkkampfer@gmail.com>
2024-06-05 12:01:43 +1000

    weather functions added
    clock.js becomes clock_weather.js
    
    async function which creates weather display
    based on clients location (lng&lat)
    
    display is initially hidden and will
    display block when click event occurs
    which also hides display of clock
    
    minor changes to index.html & style.css

---
+++ client/clock_weather.js
@@ -0,0 +1,73 @@
+
+const dateTempDiv = document.querySelector('.date_temperature')
+
+dateTempDiv.addEventListener('click', handleClick)
+
+displayClock()
+const clockClass = document.querySelector('.clock-display')
+console.log(clockClass);
+
+let weatherDisplay = document.createElement('h2')
+weatherDisplay.className = "weather-display"
+weatherDisplay.style.display = 'none'
+console.log(weatherDisplay.className);
+
+displayWeather()
+
+function handleClick(event) {
+    console.log(event.target.classList.value, 'date-temp EL');
+
+    let className = event.target.classList.value
+
+
+    if (className === "clock-display"){
+        clockClass.style.display = 'none'
+        weatherDisplay.style.display = 'block'
+    } else {
+        clockClass.style.display = 'block'
+        weatherDisplay.style.display = 'none'
+    }
+}
+
+function displayClock() {
+
+    let clockDisplay = document.createElement('h2')
+    clockDisplay.className = "clock-display"
+
+
+    setInterval(updateClock, 1000)
+    dateTempDiv.appendChild(clockDisplay)
+
+    function currentTime(){
+        return dayjs().format('ddd HH:mm:ss A')
+    }
+
+
+    function updateClock() {
+        let time = currentTime()
+        
+        clockDisplay.innerText = time
+    }
+}
+
+async function displayWeather() {
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        getWeather(position.coords.latitude, position.coords.longitude);
+    });
+
+    async function getWeather(lat, lng){
+        let url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&hourly=temperature_2m`
+       
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            
+            let temp = data.hourly.temperature_2m[0]
+            weatherDisplay.innerText = temp +  ' °C'
+            
+            dateTempDiv.appendChild(weatherDisplay)
+        })
+    }  
+}
+
